081692478d1f5bb13f0bff3965d72d1cd6bc180f
zou <1738157590@qq.com>
2024-08-05 16:19:38 +0800

    量化单独模块基础模板v0.3: 添加更清晰的随进度条打印cosine_similarity，更新运行结束打印信息

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -34,13 +34,13 @@ def model_quant(model_quant):                                                 # 
             if type(m) in [QIntLayerNorm]:
                 m.mode = 'int'
 
-def quant_stem(model_quant):
+def quant_stem(model_quant, quant=True):
     for module in model_quant.patch_embed.modules():
         # if isinstance(module, MobileOneBlock):
         #     module.quant = True
         if type(module) in [QConv2d, QLinear, QAct, QIntSoftmax]:
-                module.quant = True
-    model_quant.qact_embed.quant = True
+                module.quant = quant
+    model_quant.qact_embed.quant = quant
     # model_quant.qact_embed.register_forward_hook(hook.hook_fn)
     # for i, module in enumerate(model_quant.patch_embed):
     #     if isinstance(module, MobileOneBlock):
@@ -50,32 +50,29 @@ def quant_stem(model_quant):
 def quant_network(model_quant, quant_layers=[False, False, False, False, False, False, False, False]): # @ Zou: len(quant_layers) is 8
     for i, module in enumerate(model_quant.network):
         # print(f"network {i} module: {type(module).__name__}")
-        # network 0 module: Sequential
+        # network 0 module: Sequential   # @ Zou: stage 1
         # network 1 module: PatchEmbed
-        # network 2 module: Sequential
+        # network 2 module: Sequential   # @ Zou: stage 2
         # network 3 module: PatchEmbed
-        # network 4 module: Sequential
+        # network 4 module: Sequential   # @ Zou: stage 3
         # network 5 module: PatchEmbed
-        # network 6 module: RepCPE
-        # network 7 module: Sequential
+        # network 6 module: RepCPE       # @ Zou: stage 4 
+        # network 7 module: Sequential   # @ Zou: stage 4
         if quant_layers[i]:
             for m in module.modules():
                 if type(m) in [QConv2d, QLinear, QAct, QIntSoftmax]:
                     m.quant = True
-        # model_quant.network.register_forward_hook(hook.hook_fn)
 
-def quant_conv_exp(model_quant):
+def quant_conv_exp(model_quant, quant=True):
     for module in model_quant.conv_exp.modules():
         if type(module) in [QConv2d, QLinear, QAct, QIntSoftmax]:
-                module.quant = True
-    # model_quant.conv_exp.register_forward_hook(hook.hook_fn)
+                module.quant = quant
 
-def quant_head(model_quant):
+def quant_head(model_quant, quant=True):
     for module in model_quant.head.modules():
         if type(module) in [QConv2d, QLinear, QAct, QIntSoftmax]:
-                module.quant = True
-    model_quant.qact_out.quant = True
-    # model_quant.qact_out.register_forward_hook(hook.hook_fn)
+                module.quant = quant
+    model_quant.qact_out.quant = quant
 
 
 # @ Zou: 将hook按照FastViT的模块分为stem, network, conv_exp, head四个大部分
@@ -170,17 +167,7 @@ def calculate_cosine_similarity(hook_quant, hook_without_quant):
         # print(cosine_similarity)
     return cosine_similarity
         
-
-def print_cosine_similarity(cosine_similarities_dict):
-    for key, value in cosine_similarities_dict.items():
-        if key == "network":
-            for i, item in enumerate(value):
-                print(f"[{key}_{i}] Cosine Similarity: {item.mean().item()}")
-        else:
-            print(f"[{key}] Cosine Similarity: {value.mean().item()}")
-    
-
-
+        
 def calculate_cosine_similarity_dict(stem_hook_quant=None, stem_hook_without_quant=None,  
                                 network_hook_quant=None, network_hook_without_quant=None,
                                 conv_exp_hook_quant=None, conv_exp_hook_without_quant=None,
@@ -201,6 +188,29 @@ def calculate_cosine_similarity_dict(stem_hook_quant=None, stem_hook_without_qua
     return cosine_similarity_dict
 
 
+def print_cosine_similarity(cosine_similarities_dict):
+    for key, value in cosine_similarities_dict.items():
+        if key == "network":
+            for i, item in enumerate(value):
+                print(f"[{key}_{i}] Cosine Similarity: {item.mean().item()}")
+        else:
+            print(f"[{key}] Cosine Similarity: {value.mean().item()}")
+            
+            
+def cosine_similarities_AverageMeter_dict():
+    # stem_cosine_similarities = AverageMeter()
+    # conv_exp_cosine_similarities = AverageMeter()
+    # head_cosine_similarities = AverageMeter()
+    # cosine_similarities = {'stem': stem_cosine_similarities, 'conv_exp': conv_exp_cosine_similarities, 'head': head_cosine_similarities}
+    cosine_similarities = {}
+    cosine_similarities['stem'] = AverageMeter()
+    for i in range(8):
+        cosine_similarities[f'network_{i}'] = AverageMeter()
+    cosine_similarities['conv_exp'] = AverageMeter()
+    cosine_similarities['head'] = AverageMeter()
+    return cosine_similarities
+
+
 def validate_with_hook(args, val_loader, model_quant, model_without_quant, criterion, device):
     batch_time = AverageMeter()
     losses_quant = AverageMeter()
@@ -209,16 +219,20 @@ def validate_with_hook(args, val_loader, model_quant, model_without_quant, crite
     losses_without_quant = AverageMeter()
     top1_without_quant = AverageMeter()
     top5_without_quant = AverageMeter()
-    cosine_similarities = AverageMeter()
     top1_errors = AverageMeter()
     top5_errors = AverageMeter()
+    cosine_similarities = cosine_similarities_AverageMeter_dict()  # @ Zou: 保存不同模块的cosine similarity的字典
 
+    
     # stem_hook_quant = Hook()
     # stem_hook_without_quant = Hook()
 
     # @ Zou: place to hook and quantize modules
-    
-    quant_network(model_quant, quant_layers=[True, True, True, True, True, True, True, True])
+    quant_param = [False, [True, True, True, True, True, True, True, True], False, False] # @ Zou: stem, network, conv_exp, head的量化情况，后续可以改成从args中读取
+    quant_stem(model_quant, quant=quant_param[0])
+    quant_network(model_quant, quant_layers=quant_param[1])
+    quant_conv_exp(model_quant, quant=quant_param[2])
+    quant_head(model_quant, quant=quant_param[3])
     # switch to evaluate mode
     model_quant.eval()
     model_without_quant.eval()
@@ -230,28 +244,29 @@ def validate_with_hook(args, val_loader, model_quant, model_without_quant, crite
         target = target.to(device)
 
         with torch.no_grad():
-            # model_quant.patch_embed.register_forward_hook(stem_hook_quant.hook_fn)
-            # model_without_quant.patch_embed.register_forward_hook(stem_hook_without_quant.hook_fn)
+            # @ Zou: hook selected modules
             stem_hook_quant, stem_hook_without_quant = hook_stem(model_quant, model_without_quant)
             network_hook_quant, network_hook_without_quant = hook_network(model_quant, model_without_quant)
+            conv_exp_hook_quant, conv_exp_hook_without_quant = hook_conv_exp(model_quant, model_without_quant)
+            head_hook_quant, head_hook_without_quant = hook_head(model_quant, model_without_quant)
 
             output_quant = model_quant(data)
             output_without_quant = model_without_quant(data)
             
-            print_hooks_infor(stem_hook_quant=stem_hook_quant, stem_hook_without_quant=stem_hook_without_quant,
-                              network_hook_quant=network_hook_quant, network_hook_without_quant=network_hook_without_quant)
+            if i == 0:
+                print_hooks_infor(stem_hook_quant=stem_hook_quant, stem_hook_without_quant=stem_hook_without_quant,
+                                network_hook_quant=network_hook_quant, network_hook_without_quant=network_hook_without_quant,
+                                conv_exp_hook_quant=conv_exp_hook_quant, conv_exp_hook_without_quant=conv_exp_hook_without_quant,
+                                head_hook_quant=head_hook_quant, head_hook_without_quant=head_hook_without_quant)
             
-            # stem_feature_map_quant = stem_hook_quant.output
-            # stem_feature_map_withou_quant = stem_hook_without_quant.output
-            # stem_feature_map_quant_flat = stem_feature_map_quant.view(stem_feature_map_quant.size(0), -1)
-            # stem_hook_without_quant_flat = stem_feature_map_withou_quant.view(stem_feature_map_withou_quant.size(0), -1)
-            # cosine_similarity = F.cosine_similarity(stem_feature_map_quant_flat, stem_hook_without_quant_flat, dim=1)
             cosine_similarities_dict = calculate_cosine_similarity_dict(stem_hook_quant=stem_hook_quant, stem_hook_without_quant=stem_hook_without_quant,
-                                        network_hook_quant=network_hook_quant, network_hook_without_quant=network_hook_without_quant)
+                                        network_hook_quant=network_hook_quant, network_hook_without_quant=network_hook_without_quant,
+                                        conv_exp_hook_quant=conv_exp_hook_quant, conv_exp_hook_without_quant=conv_exp_hook_without_quant,
+                                        head_hook_quant=head_hook_quant, head_hook_without_quant=head_hook_without_quant)
 
-            print_cosine_similarity(cosine_similarities_dict)
+            # print_cosine_similarity(cosine_similarities_dict)
             
-            break
+            # break
             
         loss_quant = criterion(output_quant, target)
         loss_without_quant = criterion(output_without_quant, target)
@@ -273,33 +288,51 @@ def validate_with_hook(args, val_loader, model_quant, model_without_quant, crite
         top5_error = prec5_quant.data.item() - prec5_without_quant.data.item()
         top1_errors.update(top1_error, data.size(0))
         top5_errors.update(top5_error, data.size(0))
-        # print(f'top1: {top1.val:.3f} ({top1.avg:.3f})  top5: {top5.val:.3f} ({top5.avg:.3f}')
+       
+        cosine_similarities["stem"].update(cosine_similarities_dict["stem"].mean().item(), data.size(0))
+        cosine_similarities["conv_exp"].update(cosine_similarities_dict["conv_exp"].mean().item(), data.size(0))
+        cosine_similarities["head"].update(cosine_similarities_dict["head"].mean().item(), data.size(0))
+        for j, item in enumerate(cosine_similarities_dict["network"]):
+            cosine_similarities[f'network_{j}'].update(item.mean().item(), data.size(0))
+            
+        str_cosine_similarities = "Cosine_similarity: "
+        str_cosine_similarities += f"Stem ({cosine_similarities['stem'].avg:.4f}),"
+        for j in range(8):
+            str_cosine_similarities += f"Network_{j} ({cosine_similarities[f'network_{j}'].avg:.4f}),"
+        str_cosine_similarities += f"Conv_exp ({cosine_similarities['conv_exp'].avg:.4f}),"
+        str_cosine_similarities += f"Head ({cosine_similarities['head'].avg:.4f})"
 
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
 
+        if i % 100 == 0:   # @ Zou: 打印cosine similarity
+            loop.write(f"{i}/{len(val_loader)} " + str_cosine_similarities)
+
+
         loop.set_description('Test ')
-        loop.set_postfix_str('Time {batch_time.val:.3f} ({batch_time.avg:.3f})   '
-                            'Prec@1q ({top1q.avg:.3f})   '
-                            'Prec@5q ({top5q.avg:.3f})   '
-                            'Prec@1 ({top1.avg:.3f})   '
-                            'Prec@5 ({top5.avg:.3f})   '
-                            'Prec@1_error ({top1_error.avg:.3f})   '
-                            'Prec@5_error ({top5_error.avg:.3f})   '.format(
-                                batch_time=batch_time,
-                                lossq=losses_quant,
-                                top1q=top1_quant,
-                                top5q=top5_quant,
-                                loss=losses_without_quant,
-                                top1=top1_without_quant,
-                                top5=top5_without_quant,
-                                top1_error=top1_errors,
-                                top5_error=top5_errors,
-                            ))
+        loop.set_postfix_str(f'Time {batch_time.val:.3f} ({batch_time.avg:.3f}),' + 
+                            f'Prec@1q ({top1_quant.avg:.3f})  ' + 
+                            f'Prec@5q ({top5_quant.avg:.3f})  ' + 
+                            f'Prec@1 ({top1_without_quant.avg:.3f})  ' + 
+                            f'Prec@5 ({top5_without_quant.avg:.3f})  ' +
+                            f'Prec@1_err ({top1_errors.avg:.3f})  ' +
+                            f'Prec@5_err ({top5_errors.avg:.3f})' 
+                            )
+        
+                     
         # loop.write(f"\ncosine_similarities_stem ({cosine_similarities.avg:.4f})")
     val_end_time = time.time()
-    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Time {time:.3f}'.
-          format(top1=top1_quant, top5=top5_quant, time=val_end_time - val_start_time))
+    # print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Time {time:.3f}'.
+    #       format(top1=top1_quant, top5=top5_quant, time=val_end_time - val_start_time))
+    print(' * ' +
+            f'Time {batch_time.val:.3f} ({batch_time.avg:.3f}),' + 
+            f'Prec@1q ({top1_quant.avg:.3f})  ' + 
+            f'Prec@5q ({top5_quant.avg:.3f})  ' + 
+            f'Prec@1 ({top1_without_quant.avg:.3f})  ' + 
+            f'Prec@5 ({top5_without_quant.avg:.3f})  ' +
+            f'Prec@1_err ({top1_errors.avg:.3f})  ' +
+            f'Prec@5_err ({top5_errors.avg:.3f})\n' +
+            str_cosine_similarities)
 
     return all_reduce_mean(losses_quant.avg), all_reduce_mean(top1_quant.avg), all_reduce_mean(top5_quant.avg)
